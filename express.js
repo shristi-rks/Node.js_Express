42660fd3d1516d0ac8dfc4e693e38b8880958ec6
shristi-rks <shristi.n08@gmail.com>
2023-06-20 11:04:15 +0300

    Create GET route "/hello" that sends a JSON object

--- express.js
+++ express.js
@@ -3,8 +3,10 @@ const app = express()
 const port = 3000
 
 app.get('/hello', (req, res) => {
-  res.send('Hello World!')
-})
+        const message = { msg: 'Hello World!' };
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(message));
+  })
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
