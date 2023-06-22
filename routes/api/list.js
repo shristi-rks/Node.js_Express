da441766c6adeb7e2be41142295cf0a9390b3ff9
shristi-rks <shristi.n08@gmail.com>
2023-06-22 10:45:09 +0300

    load and save the text using 'fs' to lists.json

--- routes/api/list.js
+++ routes/api/list.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const router = express();
 const app = express();
+const fs = require('fs');
 
-const textList = []; //to store the value(text) in textList it should be outside the function
+let lists = []; //to store the value(text) in textList it should be outside the function
+
+//load the data of the existing list
+fs.readFile("./data/lists.json", "utf-8", (err, data) => {
+  if(err) {
+    console.log(err)
+    return
+  }
+  lists = JSON.parse(data);
+  console.log("Data loaded!");
+})
+
 router.post("/", (req, res) => {
   const text  = req.body.text;
-  textList.push(text);
-  console.log(textList);
-  res.json({list: textList});
+  lists.push(text);
+  
+  //Save the text in lists.json file
+  fs.writeFile("./data/lists.json",JSON.stringify(lists), err => {
+    if(err) {
+      console.log(err)
+      return
+    }
+    console.log("Data saved")
+  })
+
+  res.json({list: lists});
 });
 
 module.exports = router;
